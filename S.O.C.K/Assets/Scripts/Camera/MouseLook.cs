c2dc581465c632c010228f3abed6641bf4a31585
Epiczd <nutball37@gmail.com>
2020-02-04 17:24:22 -0500

    Clocking Out

--- S.O.C.K/Assets/Scripts/Camera/MouseLook.cs
+++ S.O.C.K/Assets/Scripts/Camera/MouseLook.cs
@@ -15,7 +15,10 @@ public class MouseLook : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        Vector3 lookAround = new Vector3(Input.GetAxis("Mouse X"), Input.GetAxis("Mouse Y"), 0f);
-        transform.position += lookAround * Time.deltaTime * sensitivity;
+        Vector3 look = new Vector3(Input.GetAxis("Mouse X"), Input.GetAxis("Mouse Y"), 0f);
+        transform.position += look * Time.deltaTime * sensitivity;
+
+        Vector3 lookAround = new Vector3(0f, 0f, Input.GetAxis("Mouse X"));
+        transform.localScale += lookAround * Time.deltaTime * sensitivity;
     }
 }
