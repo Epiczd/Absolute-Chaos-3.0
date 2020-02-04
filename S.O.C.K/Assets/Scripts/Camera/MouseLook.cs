6236d81859b98ccb4a89d4b8c3455973145c175f
Epiczd <nutball37@gmail.com>
2020-02-04 17:04:33 -0500

    MouseLook script

---
+++ S.O.C.K/Assets/Scripts/Camera/MouseLook.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseLook : MonoBehaviour
+{
+    public float sensitivity;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 lookAround = new Vector3(Input.GetAxis("Mouse X"), Input.GetAxis("Mouse Y"), 0f);
+        transform.position += lookAround * Time.deltaTime * sensitivity;
+    }
+}
