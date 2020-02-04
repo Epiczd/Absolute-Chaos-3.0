925eea9c8f53e79ce070c9cad0360ad242c2583b
Sammojr <sammojr614@gmail.com>
2020-02-04 10:39:55 -0500

    more stuff

---
+++ S.O.C.K/Assets/Scripts/U.I/Unpause.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Unpause : MonoBehaviour
+{
+   void OnMouseDown(){
+       if(Input.GetMouseButtonDown(0)){
+           Time.timeScale = 1;
+           Cursor.visible = true;
+       }
+   }
+}
