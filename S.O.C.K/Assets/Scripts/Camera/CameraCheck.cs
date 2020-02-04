d6996e125d858961981f7e4be816c7b1fdf56b0b
Epiczd <nutball37@gmail.com>
2020-02-04 16:30:04 -0500

    First and Third Person Camera

---
+++ S.O.C.K/Assets/Scripts/Camera/CameraCheck.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraCheck : MonoBehaviour
+{
+    public GameObject firstPerson;
+    public GameObject thirdPerson;
+
+    private bool firstActive;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        firstActive = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(firstActive == true)
+        {
+            firstPerson.SetActive(true);
+            thirdPerson.SetActive(false);
+        }
+        else
+        {
+            firstPerson.SetActive(false);
+            thirdPerson.SetActive(true);
+        }
+
+        if (Input.GetKey(KeyCode.F5))
+        {
+            firstActive = true;
+        }
+        else if (Input.GetKey(KeyCode.F6))
+        {
+            firstActive = false;
+        }
+    }
+}
