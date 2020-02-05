20ae2ce35f8539e40c903e1458ac562066d74e4c
Epiczd <nutball37@gmail.com>
2020-02-05 10:45:05 -0500

    Working on Character Select

---
+++ S.O.C.K/Assets/Scripts/Player/PlayerCheck.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerCheck : MonoBehaviour
+{
+    public static bool ozulActive;
+    public static bool cowboyActive;
+
+    public GameObject ozul;
+    public GameObject cowboy;
+    // Start is called before the first frame update
+    void Start()
+    {
+        if(ozulActive == true)
+        {
+            ozul.SetActive(true);
+            cowboy.SetActive(false);
+        }
+        else if (cowboyActive == true)
+        {
+            cowboy.SetActive(true);
+            ozul.SetActive(false);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ S.O.C.K/Assets/Scripts/Player/PlayerSelect.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class PlayerSelect : MonoBehaviour
+{
+    public string newLevel;
+
+    private void Update()
+    {
+        OnMouseDown();
+        Debug.Log(PlayerCheck.cowboyActive);
+    }
+
+    void OnMouseDown()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            switch (tag)
+            {
+                case "Ozul":
+                    if (gameObject.CompareTag("Ozul"))
+                    {
+                        PlayerCheck.ozulActive = true;
+                        PlayerCheck.cowboyActive = false;
+                        SceneManager.LoadScene(newLevel);
+                    }
+                    break;
+                case "Cowboy":
+                    if (gameObject.CompareTag("Cowboy"))
+                    {
+                        PlayerCheck.cowboyActive = true;
+                        PlayerCheck.ozulActive = false;
+                        SceneManager.LoadScene(newLevel);
+                    }
+                    break;
+                default:
+                    break;
+            }
+        }
+    }
+}
