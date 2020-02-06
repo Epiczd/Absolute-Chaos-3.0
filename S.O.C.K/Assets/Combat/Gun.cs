3006a5db75993546b45c3a920c3443aceda5f3e3
Epiczd <nutball37@gmail.com>
2020-02-06 09:10:58 -0500

    Combat Scripts

---
+++ S.O.C.K/Assets/Combat/Gun.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Gun : MonoBehaviour
+{
+    //Ammount of damage dealt when shooting
+    private float damage = 1f;
+
+    //Ammount of time in between each shot
+    private float invincibilityTime = 0f;
+
+    void Start()
+    {
+        
+    }
+
+    
+    void Update()
+    {
+        Debug.Log(EnemyHealth.enemyH);
+        if (Input.GetMouseButtonDown(0) && invincibilityTime <= 0f)
+        {
+            EnemyHealth.enemyH--;
+            invincibilityTime++;
+        }
+        else if (invincibilityTime > 0f)
+        {
+            invincibilityTime -= Time.deltaTime * 1.5f;
+        }
+    }
+}
---
+++ S.O.C.K/Assets/Scripts/Enemy/EnemyHealth.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyHealth : MonoBehaviour
+{
+    //Ammount of health for the enemy
+    public float health;
+
+    public static float enemyH;
+
+    public GameObject enemy;
+    
+    void Start()
+    {
+        enemyH = health;
+    }
+
+    
+    void Update()
+    {
+        if(enemyH <= 0f)
+		{
+            DestroyObject(enemy);
+		}
+    }
+}
